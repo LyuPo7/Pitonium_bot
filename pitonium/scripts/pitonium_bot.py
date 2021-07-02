8f24509c356992eeeface0ccc82c60503ae7f492
LyuPo <lport@frtk.ru>
2021-07-02 13:29:58 +0300

    Add pitonium.py

--- pitonium/scripts/pitonium_bot.py
+++ pitonium/scripts/pitonium_bot.py
@@ -5,7 +5,7 @@
 import telebot
 
 def main():
-    bot = telebot.TeleBot('TOKEN')
+    bot = telebot.TeleBot('1688182700:AAHcaPS9PZ9pS29T5TOLXNeC8BqDvCckZVg')
 
     @bot.message_handler(commands=['start', 'help'])
     def send_welcome(message):
